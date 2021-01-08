e3aa169af23422895f63d9f5d199580bb9277e7f
vivek-dev-26 <viveksai0792@gmail.com>
2021-01-08 23:00:12 +0530

    profile addition

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,14 +1,12 @@
 import React, { useState, useContext } from "react";
-import { useHistory } from "react-router-dom";
-import { FirebaseContext } from "../context/firebase";
-import { FooterContainer } from "../containers/footer";
-import { HeaderContainer } from "../containers/header";
-import { Form } from "../components";
-import * as ROUTES from "../constants/routes";
+import { BrowseContainer } from "../containers/browse";
 import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
-  const { series } = useContent('series');
-  console.log("series",series);
-  return <div>Browse</div>;
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+  const slides = selectionFilter({ series, films });
+
+  return <BrowseContainer slides={slides} />;
 }
