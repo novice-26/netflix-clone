857e2acc3326cc970d9526939fca060e512aeba2
vivek-dev-26 <viveksai0792@gmail.com>
2021-01-08 23:01:02 +0530

    minor

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -8,13 +8,7 @@ import { seedDatabase } from "../seed";
 
 //config
 
-const config = {   apiKey: "AIzaSyAXr99MdT6Dhw_qhmZT0qs3Ss4m7pdzy0Q",
-authDomain: "netflix-clone-f4c9b.firebaseapp.com",
-projectId: "netflix-clone-f4c9b",
-storageBucket: "netflix-clone-f4c9b.appspot.com",
-messagingSenderId: "491378985341",
-appId: "1:491378985341:web:0b929db5d09d0b452fa414",
-measurementId: "G-XCF6DGY62H"};
+
 
 const firebase = Firebase.initializeApp(config);
 
