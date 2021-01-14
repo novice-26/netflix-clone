896fee28f067a9e9ee3536ba946135651b29f976
vivek-dev-26 <viveksai0792@gmail.com>
2021-01-14 18:47:28 +0530

    loader added

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as Header } from "./header";
 export { default as Feature } from "./feature";
 export { default as Form } from "./form";
 export {default as Profiles} from "./profile";
+export {default as Loading} from "./loading";
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,22 @@
-import React, { useContext,useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
+import { Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  const loadingComponent = loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody/>;
+
+  return profile.displayName ? loadingComponent : <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
