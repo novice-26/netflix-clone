ffa6dae55b55ee56c7f6d621bda8fa9b6a24af04
vivek-dev-26 <viveksai0792@gmail.com>
2021-01-21 23:37:11 +0530

    test for accordion

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -22,4 +22,24 @@ describe("<Accordion/>", () => {
     expect(getByText('How do I cancel?"')).toBeTruthy();
     expect(container.firstChild).toMatchSnapShot();
   });
+
+  it("opens and closes the <Accordio/> component", () => {
+    const { container, queryByText } = render(
+      <Accordion>
+        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    );
+
+    const netflixbody =
+      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+    expect(queryByText(netflixbody)).toBeFalsy();
+    fireEvent.click(queryByText("What is Netflix?"));
+    expect(queryByText(netflixbody)).toBeTruthy();
+  });
 });
